3c75f4e477c606b88fc5f8286456924b01a508b5
arnfinn <arnfinn.steindal@gmail.com>
2019-05-13 13:16:15 +0200

    lintr fix

--- R/runscript.r
+++ R/runscript.r
@@ -3,9 +3,16 @@
 #' @export
 #'
 convert_all <- function() {
-  files <- list.files(path="json", pattern="*.json", full.names=FALSE, recursive=FALSE)
+  files <- list.files(path="json",
+                      pattern="*.json",
+                      full.names=FALSE,
+                      recursive=FALSE)
 
   lapply(files, function(x) {
-    iajson2rdata(json_file = paste("json", x, sep = "/"), rdata_file = paste0("rdata/", tools::file_path_sans_ext(x), ".RData"))
+    iajson2rdata(json_file = paste("json", x, sep = "/"),
+                 rdata_file = paste0("rdata/",
+                                     tools::file_path_sans_ext(x),
+                                    ".RData")
+                )
   })
 }
